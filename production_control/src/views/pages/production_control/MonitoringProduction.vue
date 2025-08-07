aed7e9f489c5893edc0c42eba7429b2972bc8f79
adi3947 <adi3947@gmail.com>
2025-08-08 06:32:16 +0700

    update monitoring

--- production_control/src/views/pages/production_control/MonitoringProduction.vue
+++ production_control/src/views/pages/production_control/MonitoringProduction.vue
@@ -17,81 +17,76 @@
 
     <!-- Table Container with scroll -->
     <div class="table-container">
-      <table class="my-table">
-        <thead>
-          <tr>
-            <th rowspan="3" >No</th>
-            <th rowspan="3" >Part No</th>
-            <th rowspan="3" >Job No</th>
-            <th rowspan="3" >Model</th>
-            <th rowspan="3" >Line</th>
-            <th rowspan="3" >Desc.</th>
-            <th :colspan="dates.length * 2">Schedule</th>
-            <th rowspan="3">Total</th>
-          </tr>
-          <tr>
-            <th
-              v-for="date in dates"
-              :key="date + '-header-date'"
-              colspan="2"
-            >
-              {{ date }}
-            </th>
-          </tr>
-          <tr>
-            <template v-for="date in dates" :key="date">
-              <th>D</th>
-              <th>N</th>
-            </template>
-          </tr>
-        </thead>
-        <tbody>
-          <template v-for="(row, idx) in filteredRows" :key="row.partNo">
-            <!-- DELIVERY -->
-            <tr>
-              <td :rowspan="4" >{{ idx + 1 }}</td>
-              <td :rowspan="4" >{{ row.partNo }}</td>
-              <td :rowspan="4" >{{ row.jobNo }}</td>
-              <td :rowspan="4" >{{ row.model }}</td>
-              <td :rowspan="4" >{{ row.line }}</td>
-              <td>DLV.</td>
-              <template v-for="date in dates" :key="row.partNo + date + '-dlv'">
-                <td>{{ row.data[date]?.D.dlv ?? '-' }}</td>
-                <td>{{ row.data[date]?.N.dlv ?? '-' }}</td>
-              </template>
-              <td>{{ totalDlv(row) }}</td>
-            </tr>
-            <!-- PLAN -->
-            <tr>
-              <td >PLAN</td>
-              <template v-for="date in dates" :key="row.partNo + date + '-plan'">
-                <td>{{ row.data[date]?.D.plan ?? '-' }}</td>
-                <td>{{ row.data[date]?.N.plan ?? '-' }}</td>
-              </template>
-              <td>{{ totalPlan(row) }}</td>
-            </tr>
-            <!-- ACTUAL -->
-            <tr>
-              <td>ACT.</td>
-              <template v-for="date in dates" :key="row.partNo + date + '-act'">
-                <td>{{ row.data[date]?.D.act ?? '-' }}</td>
-                <td>{{ row.data[date]?.N.act ?? '-' }}</td>
-              </template>
-              <td>{{ totalAct(row) }}</td>
-            </tr>
-            <!-- PERCENTAGE -->
-            <tr>
-              <td>%</td>
-              <template v-for="date in dates" :key="row.partNo + date + '-pct'">
-                <td>{{ pct(row.data[date]?.D) }}</td>
-                <td>{{ pct(row.data[date]?.N) }}</td>
-              </template>
-              <td>{{ totalPct(row) }}</td>
-            </tr>
+    <table class="my-table">
+      <thead>
+        <tr>
+          <th class="sticky-col1" rowspan="3">No</th>
+          <th class="sticky-col2" rowspan="3">Part No</th>
+          <th class="sticky-col3" rowspan="3">Job No</th>
+          <th class="sticky-col4" rowspan="3">Model</th>
+          <th class="sticky-col5" rowspan="3">Line</th>
+          <th class="sticky-col6" rowspan="3">Desc.</th>
+          <th :colspan="dates.length * 2">Schedule</th>
+          <th rowspan="3">Total</th>
+        </tr>
+        <tr>
+          <th v-for="date in dates" :key="date" colspan="2">
+            {{ date }}
+          </th>
+        </tr>
+        <tr>
+          <template v-for="date in dates" :key="date">
+            <th>D</th><th>N</th>
           </template>
-        </tbody>
-      </table>
-    </div>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="(row, idx) in filteredRows" :key="row.partNo">
+          <!-- DELIVERY -->
+          <tr>
+            <td class="sticky-col1" :rowspan="4">{{ idx+1 }}</td>
+            <td class="sticky-col2" :rowspan="4">{{ row.partNo }}</td>
+            <td class="sticky-col3" :rowspan="4">{{ row.jobNo }}</td>
+            <td class="sticky-col4" :rowspan="4">{{ row.model }}</td>
+            <td class="sticky-col5" :rowspan="4">{{ row.line }}</td>
+            <td class="sticky-col6">DLV.</td>
+            <template v-for="date in dates" :key="date+'dlv'">
+              <td>{{ row.data[date]?.D.dlv||'-' }}</td>
+              <td>{{ row.data[date]?.N.dlv||'-' }}</td>
+            </template>
+            <td>{{ totalDlv(row) }}</td>
+          </tr>
+          <!-- PLAN -->
+          <tr>
+            <td class="sticky-col6">PLAN</td>
+            <template v-for="date in dates" :key="row.partNo + date + '-plan'">
+              <td>{{ row.data[date]?.D.plan ?? '-' }}</td>
+              <td>{{ row.data[date]?.N.plan ?? '-' }}</td>
+            </template>
+            <td>{{ totalPlan(row) }}</td>
+          </tr>
+          <!-- ACTUAL -->
+          <tr>
+            <td class="sticky-col6">ACT.</td>
+            <template v-for="date in dates" :key="row.partNo + date + '-act'">
+              <td>{{ row.data[date]?.D.act ?? '-' }}</td>
+              <td>{{ row.data[date]?.N.act ?? '-' }}</td>
+            </template>
+            <td>{{ totalAct(row) }}</td>
+          </tr>
+          <!-- PERCENTAGE -->
+          <tr>
+            <td class="sticky-col6">%</td>
+            <template v-for="date in dates" :key="row.partNo + date + '-pct'">
+              <td>{{ pct(row.data[date]?.D) }}</td>
+              <td>{{ pct(row.data[date]?.N) }}</td>
+            </template>
+            <td>{{ totalPct(row) }}</td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
   </v-card>
 </template>
 
@@ -131,8 +126,8 @@ const rows = ref([
     model:  'SU2ID',
     line:   'SA HMMI',
     data: {
-      '01/07/25': { D: { dlv: 100, plan: 100, act: 100 }, N: { dlv: 100, plan: 100, act: 80  }},
-      '02/07/25':  { D: { dlv: 100, plan: 120, act:   0 }, N: { dlv: 100, plan: 120, act:   0 }},
+      '01/08/25': { D: { dlv: 100, plan: 100, act: 100 }, N: { dlv: 100, plan: 100, act: 80  }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act:   0 }, N: { dlv: 100, plan: 120, act:   0 }},
       // … dst
     },
     totalPlan:   1080,
@@ -144,8 +139,8 @@ const rows = ref([
     model:  'SU2ID',
     line:   'SA HMMI',
     data: {
-      '01/07/25': { D: { dlv: 100, plan:   100, act:   100 }, N: { dlv: 100, plan:   100, act: 20  }},
-      '02/07/25':  { D: { dlv: 100, plan:   100, act:   100 }, N: { dlv: 100, plan:   100, act:   100 }},
+      '01/08/25': { D: { dlv: 100, plan:   100, act:   100 }, N: { dlv: 100, plan:   100, act: 20  }},
+      '02/08/25':  { D: { dlv: 100, plan:   100, act:   100 }, N: { dlv: 100, plan:   100, act:   100 }},
       // … dst
     },
     totalPlan:   60,
@@ -157,8 +152,8 @@ const rows = ref([
     model:  'SU2ID',
     line:   'SA HMMI',
     data: {
-      '01/07/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
-      '02/07/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
       // … dst
     },
     totalPlan:   1260,
@@ -170,8 +165,151 @@ const rows = ref([
     model:  'SU2ID',
     line:   'SA HMMI',
     data: {
-      '01/07/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
-      '02/07/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
+      // … dst
+    },
+    totalPlan:   1260,
+    totalActual: 260,
+  },
+  {
+    partNo: '65197‑I7000',
+    jobNo:  '65197‑I7000',
+    model:  'SU2ID',
+    line:   'SA HMMI',
+    data: {
+      '01/08/25': { D: { dlv: 100, plan: 140, act: 140 }, N: { dlv: 100, plan: 140, act: 120 }},
+      '02/08/25':  { D: { dlv: 100, plan: 120, act: 120 }, N: { dlv: 100, plan: 120, act:  0 }},
       // … dst
     },
     totalPlan:   1260,
@@ -225,20 +363,78 @@ function clearFilter() {
 </script>
 
 <style scoped>
-.v-card { padding: 16px; }
+.v-card {
+  padding: 16px;
+  overflow: scroll;
+}
+
 .table-container {
+  /* definisikan variable dalam container yang di-scope */
+  --w1:  38.5px;
+  --w2: 108.5px;
+  --w3: 108.5px;
+  --w4: 108.5px;
+  --w5:  63px;
+  --w6:  120px;
+
+  width: calc(100vw - 50px);
+  height: calc(100vh - 50px);
   overflow: auto;
-  max-height: 600px;
+  border: 1px solid #ddd;
 }
+
 .my-table {
-  width: 100%;
-  border-collapse: collapse;
+  border-spacing: 0;
+  width: max-content;
 }
+
 .my-table th,
 .my-table td {
   border: 1px solid #ddd;
   padding: 4px 8px;
   text-align: center;
+  white-space: nowrap;
   background: #fff;
+}
+
+/* header sticky */
+.my-table thead tr:nth-child(1) th {
+  position: sticky; top: 0; z-index: 10; background: #f5f5f5;
+}
+.my-table thead tr:nth-child(2) th {
+  position: sticky; top: 34px; z-index: 9; background: #f5f5f5;
+}
+.my-table thead tr:nth-child(3) th {
+  position: sticky; top: 68px; z-index: 9; background: #f5f5f5;
+}
+
+/* kolom 1–6 sticky via class */
+.sticky-col1 {
+  position: sticky; left: 0;
+  width: var(--w1);  z-index: 7; background: #f5f5f5;
+}
+.sticky-col2 {
+  position: sticky; left: var(--w1);
+  width: var(--w2);  z-index: 6; background: #f5f5f5;
+}
+.sticky-col3 {
+  position: sticky;
+  left: calc(var(--w1) + var(--w2));
+  width: var(--w3);  z-index: 5; background: #f5f5f5;
+}
+.sticky-col4 {
+  position: sticky;
+  left: calc(var(--w1) + var(--w2) + var(--w3));
+  width: var(--w4);  z-index: 4; background: #f5f5f5;
+}
+.sticky-col5 {
+  position: sticky;
+  left: calc(var(--w1) + var(--w2) + var(--w3) + var(--w4));
+  width: var(--w5);  z-index: 3; background: #f5f5f5;
+}
+.sticky-col6 {
+  position: sticky;
+  left: calc(var(--w1) + var(--w2) + var(--w3) + var(--w4) + var(--w5) + 22.3px);
+  width: var(--w6);  z-index: 2; background: #f5f5f5;
 }
 </style>
